4dd3d39063870745b7c43f6f668a7d21d8da541e
Emily Conklin <emilyconklin31@gmail.com>
2020-04-15 20:09:08 -0600

    alts

--- src/Movie.js
+++ src/Movie.js
@@ -1,10 +1,13 @@
 import React from 'react';
 
 const Movie = ({title, director, desc, reldate, photo}) => {
+
+    const picture = ' Picture'
+    
     return (
         <div className="filmcard">
             <div className="title-contain"><h2>{title}</h2></div>
-            <div className={title}></div>
+            <div className={title} alt={title + picture}></div>
             <ul>
             <li><strong>Director:</strong> {director}</li>
             <li><strong>Release Date:</strong> {reldate}</li>
